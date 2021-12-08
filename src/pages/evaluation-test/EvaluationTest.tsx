390919a67c2ee50e3ef1df043eba7e6897c353c9
adeyemo adekunle <adeyemokay@gmail.com>
2021-12-08 07:42:38 +0100

    feat: added evaluation test link page

---
+++ src/pages/evaluation-test/EvaluationTest.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
+
+import "./evaluation-test.scss";
+const EvaluationTest = () => {
+  const [evaluationLink, setEvaluationLink] = useState(
+    "https://evaluationtest-session234-live"
+  );
+  const [copied, setCopied] = useState(false);
+  return (
+    <div className="evaluation-test">
+      <h2>Evaluation Test</h2>
+      <div className="d-flex justify-content-end align-items-center">
+        <button className="btn view-report-btn">View Report</button>
+      </div>
+      <div className="inner-container">
+        <div className="d-flex justify-content-between align-items-center link-container">
+          <p>{evaluationLink}</p>
+          <CopyToClipboard text={evaluationLink} onCopy={() => setCopied(true)}>
+            <button className="btn copy-link-btn">Copy Link</button>
+          </CopyToClipboard>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EvaluationTest;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../pages/auth/login/Login";
 import SignUp from "../pages/auth/signin/SignIn";
+import EvaluationTest from "../pages/evaluation-test/EvaluationTest";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<SignUp />} />
+        <Route path="/evaluation-test" element={<EvaluationTest />} />
       </Routes>
     </Router>
   );
