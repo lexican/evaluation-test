0f696ed36bf9b0ce0d0c20209ddd59d5695c32e5
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-12-14 06:23:20 +0100

    fix: submitAnswer func

--- src/pages/evaluation-test-question/EvaluationTestQuestion.tsx
+++ src/pages/evaluation-test-question/EvaluationTestQuestion.tsx
@@ -1,116 +1,114 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import EvaluationTestQue from "../../components/evaluation-test-quesion/EvaluationTestQue";
-import "./evaluation-test-question.style.scss";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import EvaluationTestQue from '../../components/evaluation-test-quesion/EvaluationTestQue';
+import './evaluation-test-question.style.scss';
 
 export type IQuestion = {
-  id: number;
-  question: string;
-  question_number: number;
-  question_type: string;
-  survey: number;
-  user: number;
+	id: number;
+	question: string;
+	question_number: number;
+	question_type: string;
+	survey: number;
+	user: number;
 };
 
 export type IAnswer = {
-  question: number;
-  answer: number;
+	question: number;
+	answer: number;
 };
 
 const answer: IAnswer[] = [
-  { question: 1, answer: -1 },
-  { question: 2, answer: -1 },
-  { question: 3, answer: -1 },
-  { question: 4, answer: -1 },
-  { question: 5, answer: -1 },
-  { question: 6, answer: -1 },
-  { question: 7, answer: -1 },
-  { question: 8, answer: -1 },
-  { question: 9, answer: -1 },
-  { question: 10, answer: -1 },
+	{ question: 1, answer: 0 },
+	{ question: 2, answer: 0 },
+	{ question: 3, answer: 0 },
+	{ question: 4, answer: 0 },
+	{ question: 5, answer: 0 },
+	{ question: 6, answer: 0 },
+	{ question: 7, answer: 0 },
+	{ question: 8, answer: 0 },
+	{ question: 9, answer: 0 },
+	{ question: 10, answer: 0 },
 ];
 
 const EvaluationTestQuestion = () => {
-  const [evaluationQuestions, setEvaluationQuestions] = useState<IQuestion[]>(
-    []
-  );
-  const [evaluationAnswer, setEvaluationAnswer] = useState<IAnswer[]>([
-    ...answer,
-  ]);
-  useEffect(() => {
-    getSurveyQuestion();
-  }, []);
+	const [evaluationQuestions, setEvaluationQuestions] = useState<IQuestion[]>(
+		[]
+	);
+	const [evaluationAnswer, setEvaluationAnswer] = useState<IAnswer[]>([
+		...answer,
+	]);
+	useEffect(() => {
+		getSurveyQuestion();
+	}, []);
 
-  const getSurveyQuestion = () => {
-    axios
-      .get("/surveys/questions/?survey_id=1")
-      .then((response) => {
-        setEvaluationQuestions(response.data);
-      })
-      .catch((error) => {
-        if (error.response) {
-          console.log(error.response);
-        } else if (error.request) {
-          console.log(error.request);
-        } else {
-          console.log("Error", error.message);
-        }
-        console.log(error.config);
-      });
-  };
+	const getSurveyQuestion = () => {
+		axios
+			.get('/surveys/questions/?survey_id=1')
+			.then((response) => {
+				setEvaluationQuestions(response.data);
+			})
+			.catch((error) => {
+				if (error.response) {
+					console.log(error.response);
+				} else if (error.request) {
+					console.log(error.request);
+				} else {
+					console.log('Error', error.message);
+				}
+				console.log(error.config);
+			});
+	};
 
-  const onSelect = (iQuestion: IQuestion, answer: number) => {
-    const tempAns = [...evaluationAnswer];
-    tempAns[iQuestion.question_number - 1].answer = answer;
-    setEvaluationAnswer([...tempAns]);
-  };
+	const onSelect = (iQuestion: IQuestion, answer: number) => {
+		const tempAns = [...evaluationAnswer];
+		tempAns[iQuestion.question_number - 1].answer = answer;
+		setEvaluationAnswer([...tempAns]);
+	};
 
-  const submitAnswer = () => {
-    if (evaluationAnswer.filter((e) => e.answer === -1).length > 0) {
-      alert("Fill out all questions");
-    } else {
-      console.log(evaluationAnswer);
-      axios
-        .post("surveys/evaluation/?survey_id=1", {
-          'data': evaluationAnswer,
-        })
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          if (error.response) {
-            console.log(error.response);
-          } else if (error.request) {
-            console.log(error.request);
-          } else {
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-        });
-    }
-  };
+	const submitAnswer = () => {
+		if (evaluationAnswer.filter((e) => e.answer === -1).length > 0) {
+			alert('Fill out all questions');
+		} else {
+			console.log(evaluationAnswer);
+			axios
+				.post('surveys/evaluation/?survey_id=1', evaluationAnswer)
+				.then((response) => {
+					console.log(response.data);
+				})
+				.catch((error) => {
+					if (error.response) {
+						console.log(error.response);
+					} else if (error.request) {
+						console.log(error.request);
+					} else {
+						console.log('Error', error.message);
+					}
+					console.log(error.config);
+				});
+		}
+	};
 
-  return (
-    <div className="evaluation-que">
-      <h2>Evaluation Test</h2>
-      <p className="sub-title">
-        Answer the following questions on a scale of “Strongly Disagree” to
-        “Strongly Agree”
-      </p>
-      {evaluationQuestions.map((item) => {
-        return (
-          <EvaluationTestQue
-            iQuestion={item}
-            onSelect={onSelect}
-            key={item.id}
-          />
-        );
-      })}
-      <button className="btn submit-btn" onClick={submitAnswer}>
-        Submit
-      </button>
-    </div>
-  );
+	return (
+		<div className='evaluation-que'>
+			<h2>Evaluation Test</h2>
+			<p className='sub-title'>
+				Answer the following questions on a scale of “Strongly Disagree” to
+				“Strongly Agree”
+			</p>
+			{evaluationQuestions.map((item) => {
+				return (
+					<EvaluationTestQue
+						iQuestion={item}
+						onSelect={onSelect}
+						key={item.id}
+					/>
+				);
+			})}
+			<button className='btn submit-btn' onClick={submitAnswer}>
+				Submit
+			</button>
+		</div>
+	);
 };
 
 export default EvaluationTestQuestion;
